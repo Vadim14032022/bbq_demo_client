82bdebba3b8f0aa1357dd04c5df9ce77967fa9de
Vadim <vadimrs2004@gmail.com>
2025-06-02 13:10:18 +0000

    add .gif and object lists in demo_local.py and client_local.py

--- client_local.py
+++ client_local.py
@@ -19,22 +19,25 @@ ORIENTAION_PATH = "input_data/orientation.txt"
 POSE_PATH = "input_data/pose.txt"
 #<-
 
-REMOTE_RELEVANT_OBJECTS_3D = "/home/docker_user/BeyondBareQueries/outputs/3d_relevant_objects.png"
+REMOTE_RELEVANT_OBJECTS_3D = "/home/docker_user/BeyondBareQueries/outputs/3d_relevant_objects.gif"
 REMOTE_RELEVANT_OBJECTS_2D = "/home/docker_user/BeyondBareQueries/outputs/overlayed_masks_sam_and_graph_relevant_objects.png"
 REMOTE_RELEVANT_OBJECTS_TEXT = "/home/docker_user/BeyondBareQueries/outputs/relevant_objects.txt"
+REMOTE_RELEVANT_OBJECTS_TABLE = "/home/docker_user/BeyondBareQueries/outputs/table_relevant_objects.json"
 
-REMOTE_FINAL_ANSWER_3D = "/home/docker_user/BeyondBareQueries/outputs/3d_final_answer.png"
+REMOTE_FINAL_ANSWER_3D = "/home/docker_user/BeyondBareQueries/outputs/3d_final_answer.gif"
 REMOTE_FINAL_ANSWER_2D = "/home/docker_user/BeyondBareQueries/outputs/overlayed_masks_sam_and_graph_final_answer.png"
 REMOTE_FINAL_ANSWER_TEXT = "/home/docker_user/BeyondBareQueries/outputs/final_answer.txt"
+REMOTE_FINAL_ANSWER_TABLE = "/home/docker_user/BeyondBareQueries/outputs/table_final_answer.json"
 
-LOCAL_RELEVANT_OBJECTS_3D = "3d_relevant_objects.png"
-LOCAL_RELEVANT_OBJECTS_2D = "overlayed_masks_sam_and_graph_relevant_objects.txt"
-LOCAL_RELEVANT_OBJECTS_TEXT = "relevant_objects.png"
+LOCAL_RELEVANT_OBJECTS_3D = "3d_relevant_objects.gif"
+LOCAL_RELEVANT_OBJECTS_2D = "overlayed_masks_sam_and_graph_relevant_objects.png"
+LOCAL_RELEVANT_OBJECTS_TEXT = "relevant_objects.txt"
+LOCAL_RELEVANT_OBJECTS_TABLE = "table_relevant_objects.json"
 
-LOCAL_FINAL_ANSWER_3D = "3d_final_answer.png"
+LOCAL_FINAL_ANSWER_3D = "3d_final_answer.gif"
 LOCAL_FINAL_ANSWER_2D = "overlayed_masks_sam_and_graph_final_answer.png"
 LOCAL_FINAL_ANSWER_TEXT = "final_answer.txt"
-
+LOCAL_FINAL_ANSWER_TABLE = "table_final_answer.json"
 
 def send_data(text,  ssh=None, image_path=None, depth_path=None, pose_path=None):
     with open(os.path.join(REMOTE_PATH, "text.txt"), "w") as f:
@@ -60,15 +63,17 @@ def main(args=None):
         while True:
             time.sleep(INTERVAL)
             new_timestamp = get_remote_file_timestamp(REMOTE_FINAL_ANSWER_TEXT)
-            if new_timestamp and new_timestamp != last_timestamp_final_answer:
+            if new_timestamp != last_timestamp_final_answer:
                 print(f"Change detected! Downloading {REMOTE_FINAL_ANSWER_TEXT}...")
                 shutil.copy(REMOTE_RELEVANT_OBJECTS_3D, LOCAL_RELEVANT_OBJECTS_3D)
                 shutil.copy(REMOTE_RELEVANT_OBJECTS_2D, LOCAL_RELEVANT_OBJECTS_2D)
                 shutil.copy(REMOTE_RELEVANT_OBJECTS_TEXT, LOCAL_RELEVANT_OBJECTS_TEXT)
+                shutil.copy(REMOTE_RELEVANT_OBJECTS_TABLE, LOCAL_RELEVANT_OBJECTS_TABLE)
 
                 shutil.copy(REMOTE_FINAL_ANSWER_3D, LOCAL_FINAL_ANSWER_3D)
                 shutil.copy(REMOTE_FINAL_ANSWER_2D, LOCAL_FINAL_ANSWER_2D)
                 shutil.copy(REMOTE_FINAL_ANSWER_TEXT, LOCAL_FINAL_ANSWER_TEXT)
+                shutil.copy(REMOTE_FINAL_ANSWER_TABLE, LOCAL_FINAL_ANSWER_TABLE)
                 print(f"File copied to {REMOTE_FINAL_ANSWER_TEXT}")
                 last_timestamp_final_answer = new_timestamp
                 break
--- demo_local.py
+++ demo_local.py
@@ -1,16 +1,38 @@
 import gradio as gr
 import time
 import os
+import json
 
-LOCAL_RELEVANT_OBJECTS_3D = "3d_relevant_objects.png"
-LOCAL_RELEVANT_OBJECTS_2D = "overlayed_masks_sam_and_graph_relevant_objects.txt"
-LOCAL_RELEVANT_OBJECTS_TEXT = "relevant_objects.png"
+LOCAL_RELEVANT_OBJECTS_3D = "3d_relevant_objects.gif"
+LOCAL_RELEVANT_OBJECTS_2D = "overlayed_masks_sam_and_graph_relevant_objects.png"
+LOCAL_RELEVANT_OBJECTS_TEXT = "relevant_objects.txt"
+LOCAL_RELEVANT_OBJECTS_TABLE = "table_relevant_objects.json"
 
-LOCAL_FINAL_ANSWER_3D = "3d_final_answer.png"
+LOCAL_FINAL_ANSWER_3D = "3d_final_answer.gif"
 LOCAL_FINAL_ANSWER_2D = "overlayed_masks_sam_and_graph_final_answer.png"
 LOCAL_FINAL_ANSWER_TEXT = "final_answer.txt"
+LOCAL_FINAL_ANSWER_TABLE = "table_final_answer.json"
 
 USER_QUERY = 'user_query.txt'
+
+def generate_html_table(json_data):
+    rows = ""
+    for row in json_data:
+        color = row.get("color", "#ffffff")
+        label = row.get("label", "N/A")
+        rows += f"<tr style='background-color:{color};'>"
+        rows += f"<td style='border: 1px solid #ccc; padding: 4px;'>{label}</td>"
+        rows += "</tr>"
+
+    html = f"""
+    <div style='height:400px; overflow-y:auto; border:1px solid #ccc;'>
+        <table style='width:100%; border-collapse: collapse;'>
+            <tr><th>ID: Value</th></tr>
+            {rows}
+        </table>
+    </div>
+    """
+    return html
 
 def load_image_info():
     with open(USER_QUERY, 'r') as f:
@@ -23,10 +45,17 @@ def load_image_info():
     with open(LOCAL_FINAL_ANSWER_TEXT, 'r') as f:
         final_text = f.read()
 
+    with open(LOCAL_RELEVANT_OBJECTS_TABLE, 'r') as f:
+        table1 = generate_html_table(json.load(f))
+
+    with open(LOCAL_FINAL_ANSWER_TABLE, 'r') as f:
+        table2 = generate_html_table(json.load(f))
+
     return (
-        LOCAL_RELEVANT_OBJECTS_3D, LOCAL_RELEVANT_OBJECTS_2D,
-        relevant_text, LOCAL_FINAL_ANSWER_3D, 
-        LOCAL_FINAL_ANSWER_2D, final_text, relevant_text, user_query)
+        LOCAL_RELEVANT_OBJECTS_3D, table1, LOCAL_RELEVANT_OBJECTS_2D,
+        relevant_text,
+        LOCAL_FINAL_ANSWER_3D, table2, LOCAL_FINAL_ANSWER_2D,
+        final_text, relevant_text, user_query)
 
 def monitor_and_update():
     last_mtime = None
@@ -43,17 +72,21 @@ def demo():
         gr.Markdown("# BBQ Demo")
         markdown_display = gr.Markdown()
         with gr.Row():
-            img_1_3d = gr.Image(width=400, height=400)
-            img_1_2d = gr.Image(height=400)
+            img_1_3d = gr.Image(width=400, height=400, format="gif", scale=2)
+            with gr.Column(scale=1):
+                table_1 = gr.HTML()
+            img_1_2d = gr.Image(height=400, scale=3)
         desc1 = gr.Textbox(label="Target and anchor objects")
 
-        
         with gr.Row():
-            img_2_3d = gr.Image(width=400, height=400)
-            img_2_2d = gr.Image(height=400)
+            img_2_3d = gr.Image(width=400, height=400, format="gif", scale=2)
+            with gr.Column():
+                table_2 = gr.HTML()
+            img_2_2d = gr.Image(height=400, scale=3)
         desc12 = gr.Textbox(label="Target and anchor objects")
         desc2 = gr.Textbox(label="Final answer")
-        interface.load(monitor_and_update, None, [img_1_3d, img_1_2d, desc1, img_2_3d, img_2_2d, desc2, desc12, markdown_display])
+
+        interface.load(monitor_and_update, None, [img_1_3d, table_1, img_1_2d, desc1, img_2_3d, table_2, img_2_2d, desc2, desc12, markdown_display])
     return interface
 
 demo().launch(share=True)
