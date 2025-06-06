a5ab4cd9ff248a0b0d76eada5d35c94ebc4756a3
Vadim <vadimrs2004@gmail.com>
2025-06-06 11:56:13 +0000

    update dummies, add text style

--- demo_local.py
+++ demo_local.py
@@ -17,6 +17,7 @@ LOCAL_FILES = {
 "FINAL_ANSWER_TEXT": "outputs/final_answer.txt",
 "FINAL_ANSWER_TABLE": "outputs/table_final_answer.json",
 }
+
 USER_QUERY = 'user_query.txt'
 DUMMY_QUERY = 'dummies/query.txt'
 DUMMY_GIF = "dummies/loading.gif"
@@ -53,16 +54,84 @@ def generate_html_table(json_data):
     """
     return html
 
+def format_target_and_anchors(text): #new_func
+    """Извлекаем Target and anchor objects"""
+    
+    if '{' in text and '}' in text:
+        json_part = text[text.find('{'):text.find('}')+1]
+        try:
+            data = json.loads(json_part.replace("'", '"'))
+            referred_objects = data['referred objects']
+            anchors = data['anchors']
+        except:
+            pass 
+
+    for i in range(len(referred_objects)):
+        obj, id = referred_objects[i].split(" with id ")
+        referred_objects[i] = f"{id}: {obj}"
+
+    for i in range(len(anchors)):
+        obj, id = anchors[i].split(" with id ")
+        anchors[i] = f"{id}: {obj}"
+
+    return referred_objects, anchors
+
+def format_final_answer(text): #new_func
+    """Форматирует поле Final answer"""
+    print("text: ", text)
+    if '{' in text and '}' in text:
+        json_part = text[text.find('{'):text.find('}')+1]
+        print("json_part: ", json_part)
+        try:
+            data = json.loads(json_part)
+            explanation = data['explanation']
+            id = data['id']
+        except Exception as e:
+            print("error: ", e)
+            pass
+
+    select = f"I select the object with id {id}"
+    return [select, explanation]
+
+def create_objects_html(title, list_obj1, list_obj2, name_list1, name_list2): #new func
+    """Создает HTML-блок с заголовком, targets и anchors"""
+    list_obj1_html = " ".join([f'<span class="{name_list1}">{item}</span>' for item in list_obj1])
+    list_obj2_html = " ".join([f'<span class="{name_list2}">{item}</span>' for item in list_obj2])
+    
+    return f"""
+    <div class="objects-container">
+        <h3 style="margin-bottom: 15px;">{title}</h3>
+        <div class="section-title">{name_list1}:</div>
+        <div>{list_obj1_html}</div>
+        <div class="section-title">{name_list2}:</div>
+        <div>{list_obj2_html}</div>
+    </div>
+    """
+
+def create_answer_html(title, list_obj): #new func
+    """Создает HTML-блок с заголовком, select object и explanation"""
+    return f"""
+    <div class="object-container">
+        <div class="object-title">{title}</div>
+        <div class="object-pair">
+            <span class="id-box">{list_obj[0]}</span>
+            <span class="explanation-box">{list_obj[1]}</span>
+        </div>
+    </div>
+    """
+
 def load_image_info(current_outputs):
     with open(current_outputs["USER_QUERY"], 'r') as f:
         user_query = f.read()
     user_query = f" ##  User query: {user_query}"
 
     with open(current_outputs["RELEVANT_OBJECTS_TEXT"], 'r') as f:
-        relevant_text = f.read()
+        #relevant_text = f.read()
+        targets, anchors = format_target_and_anchors(f.read()) #modified string
 
     with open(current_outputs["FINAL_ANSWER_TEXT"], 'r') as f:
-        final_text = f.read()
+        #final_text = f.read()
+        answer = format_final_answer(f.read()) #modified string
 
     with open(current_outputs["RELEVANT_OBJECTS_TABLE"], 'r') as f:
         table1 = generate_html_table(json.load(f))
@@ -72,9 +141,10 @@ def load_image_info(current_outputs):
 
     return (
         current_outputs["RELEVANT_OBJECTS_3D"], table1, current_outputs["RELEVANT_OBJECTS_2D"],
-        relevant_text,
+        create_objects_html("Target and anchors (deductive stage 1)", targets, anchors, "Targets", "Anchors"), #modified string
         current_outputs["FINAL_ANSWER_3D"], table2, current_outputs["FINAL_ANSWER_2D"],
-        final_text, relevant_text, user_query)
+        create_answer_html("Final answer (deductive stage 2)", answer), #modified string
+        user_query)
 
 def get_local_file_timestamp(local_path):
     if os.path.exists(local_path):
@@ -87,28 +157,35 @@ def monitor_and_update():
     yield load_image_info(DUMMY_OUTPUTS)
     while True:
         time.sleep(INTERVAL)
+        print(f"{query_mtimes} VS {get_local_file_timestamp(USER_QUERY)}")
         if query_mtimes != get_local_file_timestamp(USER_QUERY):
             query_mtimes = get_local_file_timestamp(USER_QUERY)
-            current_outputs = DUMMY_OUTPUTS
+            current_outputs = DUMMY_OUTPUTS.copy()
             current_outputs["USER_QUERY"] = USER_QUERY
             yield load_image_info(current_outputs)
+            break_out = set()
             while True:
                 updated = False
-                break_out = True
+                time.sleep(INTERVAL/2)
                 for key, path in LOCAL_FILES.items():
                     current_mtime = get_local_file_timestamp(path)
                     if current_mtime != last_mtimes[key]:
                         last_mtimes[key] = current_mtime
                         current_outputs[key] = path
                         updated = True
-                    break_out &= (current_mtime != last_mtimes[key])
+                        break_out.add(path)
+                print("break_out ", break_out)
+                if len(break_out) == len(LOCAL_FILES):
+                    break
                 if updated:
                     yield load_image_info(current_outputs)
-                if break_out:
-                    break
+            yield load_image_info(current_outputs)
+
+with open("demo_css.txt", "r") as f:
+    css = f.read()
 
 def demo():
-    with gr.Blocks(fill_height=True) as interface:
+    with gr.Blocks(css=css, fill_height=True) as interface:
         gr.Markdown("# BBQ Demo")
         markdown_display = gr.Markdown()
         with gr.Row():
@@ -116,17 +193,16 @@ def demo():
             with gr.Column(scale=1):
                 table_1 = gr.HTML()
             img_1_2d = gr.Image(height=400, scale=3)
-        desc1 = gr.Textbox(label="Target and anchor objects")
+        objects_section_1 = gr.HTML()
 
         with gr.Row():
             img_2_3d = gr.Image(width=400, height=400, format="gif", scale=2)
             with gr.Column():
                 table_2 = gr.HTML()
             img_2_2d = gr.Image(height=400, scale=3)
-        desc12 = gr.Textbox(label="Target and anchor objects")
-        desc2 = gr.Textbox(label="Final answer")
+        objects_section_2 = gr.HTML()
 
-        interface.load(monitor_and_update, None, [img_1_3d, table_1, img_1_2d, desc1, img_2_3d, table_2, img_2_2d, desc2, desc12, markdown_display])
+        interface.load(monitor_and_update, None, [img_1_3d, table_1, img_1_2d, objects_section_1, img_2_3d, table_2, img_2_2d, objects_section_2, markdown_display])
     return interface
 
 demo().launch(share=True)
